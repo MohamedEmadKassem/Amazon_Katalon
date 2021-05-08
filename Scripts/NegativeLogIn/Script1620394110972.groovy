f49959bceb8ab071ce327d3f49520ed45f1836bc
MohamedKassem95 <80948494+MohamedEmadKassem@users.noreply.github.com>
2021-05-08 16:06:33 +0300

    delay

--- Scripts/NegativeLogIn/Script1620394110972.groovy
+++ Scripts/NegativeLogIn/Script1620394110972.groovy
@@ -21,22 +21,38 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://www.amazon.com.tr/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.tr%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=trflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
 
+WebUI.delay(1)
+
 WebUI.setText(findTestObject('Object Repository/Page_Amazon Giri Yap/input_E-posta adresi veya telefon numaras_email'), 
     'mohamed1995')
 
+WebUI.delay(1)
+
 WebUI.click(findTestObject('Object Repository/Page_Amazon Giri Yap/input_E-posta adresinizi veya cep telefonu _e246cd'))
+
+WebUI.delay(1)
 
 WebUI.setText(findTestObject('Object Repository/Page_Amazon Giri Yap/input_E-posta adresi veya telefon numaras_email'), 
     'emad.1995@gmail.com')
 
+WebUI.delay(1)
+
 WebUI.click(findTestObject('Object Repository/Page_Amazon Giri Yap/input_E-posta adresinizi veya cep telefonu _e246cd'))
+
+WebUI.delay(1)
 
 WebUI.setText(findTestObject('Object Repository/Page_Amazon Giri Yap/input_E-posta adresi veya telefon numaras_email'), 
     'mohamed.2.kassem95@gmail.com')
 
+WebUI.delay(1)
+
 WebUI.click(findTestObject('Object Repository/Page_Amazon Giri Yap/input_E-posta adresinizi veya cep telefonu _e246cd'))
 
+WebUI.delay(1)
+
 WebUI.setEncryptedText(findTestObject('Object Repository/Page_Amazon Giri Yap/input_ifremi unuttum_password'), 'DB2lcEajKzw3X4cw8iI91A==')
+
+WebUI.delay(1)
 
 WebUI.click(findTestObject('Object Repository/Page_Amazon Giri Yap/input_ifrenizi girin_signInSubmit'))
 
