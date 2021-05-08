34cbe4181557c4df8e4f2b838d1aee6f07a6b982
MohamedKassem95 <80948494+MohamedEmadKassem@users.noreply.github.com>
2021-05-08 19:35:51 +0300

    Marka

---
+++ Object Repository/Marka/Page_Amazon.com.tr  usb kablo/i_Hama_a-icon a-icon-extender-expand.rs
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>i_Hama_a-icon a-icon-extender-expand</name>
+   <tag></tag>
+   <elementGuidId>72228ef3-14af-4806-83f9-f8559751b7f2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='brandsRefinements']/ul/li[8]/span/div/a/i</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#brandsRefinements > ul.a-unordered-list.a-nostyle.a-vertical.a-spacing-medium > li.a-spacing-micro > span.a-list-item > div.a-row.a-expander-container.a-expander-extend-container > a.a-expander-header.a-declarative.a-expander-extend-header.s-expander-text > i.a-icon.a-icon-extender-expand</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>i</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>a-icon a-icon-extender-expand</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;brandsRefinements&quot;)/ul[@class=&quot;a-unordered-list a-nostyle a-vertical a-spacing-medium&quot;]/li[@class=&quot;a-spacing-micro&quot;]/span[@class=&quot;a-list-item&quot;]/div[@class=&quot;a-row a-expander-container a-expander-extend-container&quot;]/a[@class=&quot;a-expander-header a-declarative a-expander-extend-header s-expander-text&quot;]/i[@class=&quot;a-icon a-icon-extender-expand&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='brandsRefinements']/ul/li[8]/span/div/a/i</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[4]/ul/li[8]/span/div/a/i</value>
+   </webElementXpaths>
+</WebElementEntity>
