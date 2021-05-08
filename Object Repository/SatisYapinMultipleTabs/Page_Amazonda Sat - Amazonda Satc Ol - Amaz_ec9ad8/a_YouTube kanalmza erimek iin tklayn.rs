d1dde4976d02fb00ede94cade914bd4ef27f29e9
MohamedKassem95 <80948494+MohamedEmadKassem@users.noreply.github.com>
2021-05-08 16:37:35 +0300

    SatisYapinMultipleTabs

---
+++ Object Repository/SatisYapinMultipleTabs/Page_Amazonda Sat - Amazonda Satc Ol - Amaz_ec9ad8/a_YouTube kanalmza erimek iin tklayn.rs
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_YouTube kanalmza erimek iin tklayn</name>
+   <tag></tag>
+   <elementGuidId>dd756fe3-dbf7-45fd-b278-5b2a3706c349</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='YOUTUBEPOPUP']/div/div[2]/div[5]/div/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.padding-top-mini.padding-bottom-mini.flex-container.flex-align-items-stretch.flex-align-content-flex-start.flex-full-width.amsg-2018.border-color-transparent.fonts-loaded > div.align-center > a.link.button.button-type-primary.button-theme-filled.font-size-medium.ember</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://www.youtube.com/channel/UCt-GISrm4Ha_FikMVTdRc0Q?initialSessionID=262-1496031-5844412&amp;ld=ELTRSOA-www.amazon.com.tr</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>link  button button-type-primary button-theme-filled font-size-medium ember</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>target</name>
+      <type>Main</type>
+      <value>_blank</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>YouTube kanalımıza erişmek için tıklayın</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;YOUTUBEPOPUP&quot;)/div[@class=&quot;popup&quot;]/div[@class=&quot;max-width-800px popup-container&quot;]/div[@class=&quot;padding-top-mini padding-bottom-mini flex-container flex-align-items-stretch flex-align-content-flex-start flex-full-width amsg-2018 border-color-transparent fonts-loaded&quot;]/div[@class=&quot;align-center&quot;]/a[@class=&quot;link  button button-type-primary button-theme-filled font-size-medium ember&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='YOUTUBEPOPUP']/div/div[2]/div[5]/div/a</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'YouTube kanalımıza erişmek için tıklayın')]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Amazon.com.tr Satıcı Eğitim Programı artık YouTube’da'])[1]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='✕'])[1]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='✕'])[2]/preceding::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Koşullar:'])[1]/preceding::a[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='YouTube kanalımıza erişmek için tıklayın']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://www.youtube.com/channel/UCt-GISrm4Ha_FikMVTdRc0Q?initialSessionID=262-1496031-5844412&amp;ld=ELTRSOA-www.amazon.com.tr')]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div[5]/div/a</value>
+   </webElementXpaths>
+</WebElementEntity>
